d8298215459f2e8a1814fd4ba988c9e35c01ff55
Yahyobek <155903617+dreamdev-MYU@users.noreply.github.com>
2024-04-25 22:10:49 +0500

    next commit

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import SignUpApiView, VerifyCodeApiView
 urlpatterns = [
     path('sign-up/', SignUpApiView.as_view(), name='SignUp')
-    path("verify-code/", VerifyCodeApiView.as_view(), name="verify_code")
+    
 
 ]
 
